d8d0657c9ad9ebed0ad402120a7d2a1fd5831e43
Barchinoy0803 <barchinoyyusupova33@gmail.com>
2025-05-18 20:35:57 +0500

    some features updated

--- src/components/pagination/index.jsx
+++ src/components/pagination/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Pagination from '@mui/material/Pagination';
+import { useStateValue } from '@/context';
 
 const PaginationP = ({ currentPage, totalPages, onPageChange }) => {
+  const [state] = useStateValue()
   const handleChange = (event, value) => {
     onPageChange(value);
   };
@@ -17,6 +19,21 @@ const PaginationP = ({ currentPage, totalPages, onPageChange }) => {
       boundaryCount={1}
       showFirstButton
       showLastButton
+      sx={{
+        backgroundColor: state.isDarkmode ? '#1e1e1e' : '#fff',
+        color: state.isDarkmode ? '#d3d3d3' : '#000',
+        borderRadius: 2,
+        padding: '8px',
+        '& .MuiPaginationItem-root': {
+          color: state.isDarkmode ? '#fff' : '#000',
+          borderColor: state.isDarkmode ? '#000' : '#ccc',
+        },
+        '& .Mui-selected': {
+          backgroundColor: state.isDarkmode ? '#333' : '#ddd',
+          color: state.isDarkmode ? '#fff' : '#000',
+        },
+      }}
+
     />
   );
 };
